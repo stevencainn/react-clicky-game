c5d3f8c70a4948706977d9faee7744dc533ebe4e
steven cain <stevencainn@gmail.com>
2020-01-26 13:48:13 -0500

    Updates

---
+++ precache-manifest.dcb0819e5d11dcc14bf02fc2c7af7258.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80dded7294c4142add3236448a627de",
+    "url": "/stevencainn/react-clicky-game/index.html"
+  },
+  {
+    "revision": "e86330668eb6d1112886",
+    "url": "/stevencainn/react-clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2b5cef4b02b578047051",
+    "url": "/stevencainn/react-clicky-game/static/js/2.d811cc5e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stevencainn/react-clicky-game/static/js/2.d811cc5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e86330668eb6d1112886",
+    "url": "/stevencainn/react-clicky-game/static/js/main.054d3736.chunk.js"
+  },
+  {
+    "revision": "deac44021d7cf70b86ac",
+    "url": "/stevencainn/react-clicky-game/static/js/runtime-main.b33a2449.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stevencainn/react-clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
